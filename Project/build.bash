d7224ff506aacc43ebe11c40b8a63a9a6d05d65f
Username-Leon <leonardo.temperanza@gmail.com>
2023-03-28 13:36:48 +0200

    First commit

--- Project/build.bash
+++ Project/build.bash
@@ -6,9 +6,6 @@ fi
 
 pushd ../Build
 
-ls
-
-
 # TODO: problems with linkage. Maybe i just need the correct version of LLVM or something
 # also suppress the 100000 warnings
 clang -I ../Project/Source -o Constellate ../Project/Source/unity_build.cpp
